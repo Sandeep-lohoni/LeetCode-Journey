d0a4230b98af8818692852b6c4bd58617fc64756
Sandeep8UI <sandeeplohani333@gmail.com>
2024-04-16 16:01:17 +0530

    first commit

---
+++ generate_parenthesis_recursion.cpp
@@ -0,0 +1,49 @@
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+// Example 1:
+
+// Input: n = 3
+// Output: ["((()))","(()())","(())()","()(())","()()()"]
+// Example 2:
+
+// Input: n = 1
+// Output: ["()"]
+ 
+
+// Constraints:
+
+// 1 <= n <= 8
+
+// solution
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<string> v ;
+    void generate(string & s,int open,int close){
+        if(open==0 && close==0){
+            v.push_back(s);
+            return;
+        }
+        if(open>0){
+            s.push_back('(');
+            generate(s,open-1,close);
+            s.pop_back();
+        }
+        if(close>0){
+            if(open<close){
+                s.push_back(')');
+                generate(s,open,close-1);
+                s.pop_back();
+            }
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        string s;
+        generate(s,n,n);
+        return v;
+    }
+};
---
+++ subsets_recursive.cpp
@@ -0,0 +1,47 @@
+// Given an integer array nums of unique elements, return all possible 
+// subsets
+//  (the power set).
+
+// The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [[],[0]]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 10
+// -10 <= nums[i] <= 10
+// All the numbers of nums are unique.
+
+// solution
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+vector<vector<int>> sets;
+   void generate(vector<int> sub, int i, vector<int>& nums){
+            if(i==nums.size()){
+                sets.push_back(sub);
+                return;
+            }
+            generate(sub,i+1,nums);
+            sub.push_back(nums[i]);
+            generate(sub, i+1, nums);
+            sub.pop_back();
+   }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> empty;
+        generate(empty,0,nums);
+        return sets;
+    }
+};
