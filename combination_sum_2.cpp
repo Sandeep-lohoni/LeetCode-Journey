95d7cfd256e6e26558e05d0796b0bc0a06ace05c
Sandeep8UI <sandeeplohani333@gmail.com>
2024-04-17 15:57:45 +0530

    first commit

---
+++ combination_sum_2.cpp
@@ -0,0 +1,61 @@
+// Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+// Each number in candidates may only be used once in the combination.
+
+// Note: The solution set must not contain duplicate combinations.
+
+ 
+
+// Example 1:
+
+// Input: candidates = [10,1,2,7,6,1,5], target = 8
+// Output: 
+// [
+// [1,1,6],
+// [1,2,5],
+// [1,7],
+// [2,6]
+// ]
+// Example 2:
+
+// Input: candidates = [2,5,2,1,2], target = 5
+// Output: 
+// [
+// [1,2,2],
+// [5]
+// ]
+ 
+
+// Constraints:
+
+// 1 <= candidates.length <= 100
+// 1 <= candidates[i] <= 50
+// 1 <= target <= 30
+
+// solution
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void helper(int index,int target, vector<vector<int>> &ans,vector<int>& candidates,vector<int>&ds){
+        if(target==0){
+            ans.push_back(ds);
+            return;
+        }
+        for(int i=index;i<candidates.size();++i){
+            if(i>index && candidates[i]==candidates[i-1]) continue;
+            if(candidates[i]>target) break;
+            ds.push_back(candidates[i]);
+            helper(i+1,target-candidates[i],ans,candidates,ds);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>>ans;
+        vector<int>ds;
+        helper(0,target,ans,candidates,ds);
+        return ans;
+    }
+};
